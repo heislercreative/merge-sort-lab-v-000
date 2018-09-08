f88da9a894a29ef5acfadd60fa7cdb2c9463c4f2
Brennan Heisler <learn@heislercreative.com>
2018-09-08 20:55:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function findMinAndRemoveSorted(array) {
   return array.shift()
 }
 
+<<<<<<< HEAD
 
 function merge(array1, array2) {
   let sorted = []
@@ -27,4 +28,21 @@ function mergeSort(array) {
   } else {
     return merge(mergeSort(firstHalf), mergeSort(secondHalf))
   }
+=======
+function merge(array1, array2) {
+  let sorted = []
+  let currentMin
+  
+  while (array1.length != 0 && array2.length != 0) {
+    let minArray1 = findMinAndRemoveSorted(array1)
+    let minArray2 = findMinAndRemoveSorted(array2)
+    if (minArray1 < minArray2) {
+      currentMin = minArray1
+    } else {
+      currentMin = minArray2
+    }
+    sorted.push(currentMin)
+  }
+  return sorted.concat(minArray1).concat(minArray2)
+>>>>>>> ce911c66867d2f5548bf2b45050c9f50165955bf
 }
